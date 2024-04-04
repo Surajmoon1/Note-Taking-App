ac36311051dde678637cbb093ed73459fe99049a
Suraj Moon <surajmoon78725@gmail.com>
2024-04-04 10:59:33 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const button = document.querySelector('#btn');
-
+const button = document.querySelector("#btn");
 
 const saveNotes = () => {
-    const notes = document.querySelectorAll('.text')
-    let data = [];
-    notes.forEach(
-        (note) => {
-            data.push(note.value)
-        }
-    )
-    console.log(data)
+  const notes = document.querySelectorAll(".text");
+  let data = [];
+  notes.forEach((note) => {
+    data.push(note.value);
+  });
+  //   console.log(data);
 
-    if (data.length === 0) {
-        localStorage.removeItem('notes')
-    } else {
-        localStorage.setItem('notes', JSON.stringify(data))
-    }
-}
+  if (data.length === 0) {
+    localStorage.removeItem("notes");
+  } else {
+    localStorage.setItem("notes", JSON.stringify(data));
+  }
+};
 
-
-const addNote = (text = '') => {
-    let note = document.createElement('div')
-    note.classList.add('note')
-    note.innerHTML = `
+const addNote = (text = "") => {
+  let note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
      <div class="toolbar">   
          <i class="save fa-solid fa-floppy-disk"></i>
          <i class="delete fa-solid fa-trash-can"></i>
@@ -30,53 +26,43 @@ const addNote = (text = '') => {
      <textarea class="text">${text}</textarea>
     `;
 
-    note.querySelector('.delete').addEventListener('click', () => {
-        confirmDelete = confirm('Do you Really wanna Delete ?')
-        if (confirmDelete == true) {
-            note.remove()
-        } 
-        saveNotes()
-
-    })
-
-    note.querySelector('.save').addEventListener('click', function () {
-        saveNotes()
-    })
-
-    note.querySelector('.text').addEventListener("focusout", function () {
-        saveNotes()
-    })
-
-    document.querySelector('#main').prepend(note)
-
-    saveNotes()
-}
-
-
-
-(
-    function () {
-        const lsnote = JSON.parse(localStorage.getItem('notes')) || []
-
-        if (lsnote.length === 0) {
-            localStorage.removeItem('notes')
-        } else {
-            // addNote()
-        }
-
-        lsnote.forEach(
-            (lsnotes) => {
-                addNote(lsnotes)
-            }
-        )
+  note.querySelector(".delete").addEventListener("click", () => {
+    undo = confirm("Do you Really wanna Delete ?");
+    if (undo == true) {
+      note.remove();
     }
-)()
+    saveNotes();
+  });
 
-button.addEventListener('click', function () {
-    addNote()
-})
+  note.querySelector(".save").addEventListener("click", function () {
+    saveNotes();
+  });
 
-addNote()
+  note.querySelector(".text").addEventListener("focusout", function () {
+    saveNotes();
+  });
 
+  document.querySelector("#main").prepend(note);
 
+  // saveNotes()
+};
 
+(function () {
+  const lsnote = JSON.parse(localStorage.getItem("notes")) || [];
+
+  if (lsnote.length === 0) {
+    localStorage.removeItem("notes");
+  } else {
+    // addNote()
+  }
+
+  lsnote.forEach((lsnotes) => {
+    addNote(lsnotes);
+  });
+})();
+
+button.addEventListener("click", function () {
+  addNote();
+});
+
+addNote();
